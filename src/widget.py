ba2d4679d0836bb203598efb74de73c75f1425b3
serz <serzkuh@yandex.ru>
2025-01-23 07:40:21 +0500

    change main fanction

--- src/widget.py
+++ src/widget.py
@@ -3,11 +3,11 @@ from typing import Union
 from src.masks import get_mask_card_number,get_mask_account
 
 
-def mask_account_card(cartdate: Union[str],bankaccount: Union[str]):
-    """ функция возвращает замаскированные счет и номер банковской карты """
+def mask_account_card(cart_count_date: Union[str]):
+    """ функция возвращает замаскированные счет или номер банковской карты """
     letter_1 = []
     number_1 = []
-    for i in cartdate:
+    for i in cart_count_date:
         if i == " " or i.isalpha() == True:
             letter_1.append(i)
         if i.isnumeric() == True:
@@ -18,26 +18,11 @@ def mask_account_card(cartdate: Union[str],bankaccount: Union[str]):
 
     res_1 = letter_1_1 + " " + get_mask_card_number(number_1_1)
     print(res_1)
-    #return res_1
-
-    letter_2 = []
-    number_2 = []
-    for i in bankaccount:
-        if i==" " or i.isalpha() == True:
-            letter_2.append(i)
-        if i.isnumeric() == True:
-            number_2.append(i)
-
-    letter_2_2 = ''.join(letter_2)
-    number_2_2 = ''.join(number_2)
-
-    res_2 = letter_2_2 + " " + get_mask_account(number_2_2)
-    print(res_2)
-
     return res_1
-    return res_2
 
-mask_account_card('Visa Platinum 7000792289606361','Счет 73654108430135874305')
+
+
+mask_account_card('Visa Platinum 7000792289606361')
 
 
 def get_time(timest: Union[str]):
