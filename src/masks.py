e809a28e2d10a5c8518a748553c252c2fc8314df
serz <serzkuh@yandex.ru>
2025-01-24 03:05:04 +0500

    changed main function

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 
-def get_mask_card_number(cartdate: Union[str]) ->Union[str]  :
+def get_mask_card_number(cartdate: Union[str]) -> Union[str]:
     """функция возвращает скрытый номер банковской карты"""
     new_cartdate = (
         cartdate[0:4]
@@ -13,10 +13,11 @@ def get_mask_card_number(cartdate: Union[str]) ->Union[str]  :
         + " "
         + cartdate[12:16]
     )
+
     return new_cartdate
 
 
-def get_mask_account(bankaccount: Union[str]) ->Union[str]:
+def get_mask_account(bankaccount: Union[str]) -> Union[str]:
     """функция возвращает  скрытый номер банковского счета"""
     new_bankaccount = len(bankaccount[14:16]) * "*" + bankaccount[16:20]
     return new_bankaccount
--- src/widget.py
+++ src/widget.py
@@ -1,32 +1,30 @@
-from datetime import *
 from typing import Union
-from src.masks import get_mask_card_number,get_mask_account
 
 
-def mask_account_card(cart_count_date: Union[str]):
+def mask_counter(cart_count_date: Union[str]):
     """ функция возвращает замаскированные счет или номер банковской карты """
-    letter_1 = []
-    number_1 = []
+
+    letter_ = []
+    number_ = []
+
     for i in cart_count_date:
         if i == " " or i.isalpha() == True:
-            letter_1.append(i)
+            letter_.append(i)
         if i.isnumeric() == True:
-            number_1.append(i)
+            number_.append(i)
 
-    letter_1_1 = ''.join(letter_1)
-    number_1_1 = ''.join(number_1)
+    letter_1 = ''.join(letter_)
+    number_1 = ''.join(number_)
 
-    res_1 = letter_1_1 + " " + get_mask_card_number(number_1_1)
-    print(res_1)
-    return res_1
+    res = str(letter_1) + " " + str(number_1)
+    print(res)
+    return res
 
-
-
-mask_account_card('Visa Platinum 7000792289606361')
-
+mask_counter('Visa Platinum 7000792289606361')
 
 def get_time(timest: Union[str]):
     """ функция изменяет формат строки в обьекте Data """
+
     timestr_ = list(timest.split('T'))
     timestr_1 = list(timestr_[0].split('-'))
     timestr_1.reverse()
@@ -36,4 +34,3 @@ def get_time(timest: Union[str]):
 
 
 get_time('2018-11-07T13:12:05.485858')
-
