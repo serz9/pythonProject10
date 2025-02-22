9333f5ce867cc1cc1e3a303186054995966ddc01
serz9 <serzkuh@yandex.ru>
2025-02-23 03:15:02 +0500

    some modification

--- src/processing.py
+++ src/processing.py
@@ -5,7 +5,7 @@ def filter_by_state(dictioneris: list[dict[str, Any]], state=None):
 
     """ функция сортировки словаря """
 
-    dictioneris_1 = []
+    dictioneris_1: list = []
 
     for i in dictioneris:
         if i['state'] == state:
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
 from typing import Union
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
