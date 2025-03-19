ee0621b252310fb4cea57e5c5e3296fc0480da27
serz9 <serzkuh@yandex.ru>
2025-03-20 04:58:01 +0500

    some addings

--- src/masks.py
+++ src/masks.py
@@ -5,6 +5,13 @@ def get_mask_card_number(cartdate: Union[str]) -> Union[str]:
 
     """функция возвращает скрытый номер банковской карты"""
 
+    if len(cartdate) > 16:
+        print(" Вы ввели количество символов более необходимого ")
+    if len(cartdate) < 16:
+        print(" Вы ввели  менее необходимого количество символов ")
+
+
+
     new_cartdate = (
             cartdate[0:4]
             + " "
@@ -15,6 +22,8 @@ def get_mask_card_number(cartdate: Union[str]) -> Union[str]:
             + " "
             + cartdate[12:16]
     )
+
+    print(new_cartdate)
     return new_cartdate
 
 
@@ -25,3 +34,7 @@ def get_mask_account(bankaccount: Union[str]) -> Union[str]:
     new_bankaccount = len(bankaccount[14:16]) * "*" + bankaccount[16:20]
     print(new_bankaccount)
     return new_bankaccount
+
+
+get_mask_card_number('8384888559998899')
+get_mask_account('83848885599988999879')
