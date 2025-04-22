3598aacba72a613326ed957d94022abba66d3bde
serz9 <serzkuh@yandex.ru>
2025-04-23 01:26:46 +0500

    add test_mycod

--- tests/test_mycod.py
+++ tests/test_mycod.py
@@ -1,11 +1,12 @@
 import pytest
-from generators.generators import filter_by_carrency,transaction_descriptions
+from generators.generators import filter_by_carrency,transaction_descriptions,tranzactions_list
 
-def test_filter_by_carrency(test_tranzact_list):
+def test_filter_by_carrency(filter_by_carrency):
 
     """  тест фильтрации данных  """
 
-    res_ = list(test_filter_by_carrency(test_tranzact_list))
+    res_ = list(filter_by_carrency(tranzactions_list))
+    print(res)
     expected == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
 {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
     assert res_ == expected
@@ -13,7 +14,7 @@ def test_filter_by_carrency(test_tranzact_list):
 
 
 
-def test_transaction_descriptions(tranzact_list,transaction_descriptions):
+def test_transaction_descriptions(transaction_descriptions):
 
     res_ = transactions_discription(tranzact_list)
     assert res_ == ['Перевод организации', 'Перевод со счета на счет']
