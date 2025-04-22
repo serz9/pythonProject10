03cb938619928a03c0091019c218a542d5931007
serz9 <serzkuh@yandex.ru>
2025-04-23 01:03:47 +0500

    add test_mycod

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,23 @@
 import pytest
+from generators.generators import tranzactions_list
 
 
+def filter_by_carrency(tranzactions_list: list[str], currency: [str]):
+
+    """   функция фильтрации   """
+
+    res = (i for i in tranzactions_list if i['operationAmount']['currency']['code'] == currency)
+    for n in res:
+        yield n
+
+
+filter_run = filter_by_carrency(tranzactions_list, 'USD')
+for tranzaction in filter_run:
+    print(tranzaction)
+
+filter_by_carrency(tranzactions_list, 'USD')
 @pytest.fixture
-def tranzact_list():
+def test_tranzact_list():
      return  [
            {
             "id": 939719570,
@@ -81,21 +96,7 @@ def tranzact_list():
  #                   "name": 'USD',
  #                   "code": "U"
  #               }
- #           },
-  #          "description": "Перевод со счета на счет",
-  #          "from": "Счет 19708645243227258542",
-  #          "to": "Счет 75651667383060284188"
-  #      },
-  #     {
-  #      "id": 142264268,
-  #      "state": "EXECUTED",
-  #      "date": "2019-04-04T23:20:05.206878",
-  #      "operationAmount": {
- #       "amount": "79114.93",
- #           "currency": {
-  #              "name": 'USD',
-  #              "code": "U"
-  #          }
+ #           }
 
 
 
--- tests/test_mycod.py
+++ tests/test_mycod.py
@@ -1,11 +1,11 @@
 import pytest
 from generators.generators import filter_by_carrency,transaction_descriptions
 
-def test_filter_by_carrency(tranzact_list):
+def test_filter_by_carrency(test_tranzact_list):
 
     """  тест фильтрации данных  """
 
-    res_ = list(filter_by_carrency(tranzact_list,'USD'))
+    res_ = list(test_filter_by_carrency(test_tranzact_list))
     expected == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
 {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
     assert res_ == expected
