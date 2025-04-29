1fd79f3f4ddbd7be295d04c146a5a7ef125a09c4
serz9 <serzkuh@yandex.ru>
2025-04-30 01:54:06 +0500

    some addings

--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ from typing import Union
 
 def get_mask_card_number(cartdate: Union[str]) -> Union[str]:
 
-    """функция возвращает скрытый номер банковской карты"""
+    """   Функция возвращает скрытый номер банковской карты   """
 
     new_cartdate = (
             cartdate[0:4]
@@ -20,7 +20,7 @@ def get_mask_card_number(cartdate: Union[str]) -> Union[str]:
 
 def get_mask_account(bankaccount: Union[str]) -> Union[str]:
 
-    """функция возвращает  скрытый номер банковского счета"""
+    """   Функция возвращает  скрытый номер банковского счета   """
 
     new_bankaccount = len(bankaccount[14:16]) * "*" + bankaccount[16:20]
     print(new_bankaccount)
