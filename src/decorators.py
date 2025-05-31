33c50766b732ad734700b720516fc53404344155
serz9 <serzkuh@yandex.ru>
2025-06-01 02:28:22 +0500

    create decorator

--- src/decorators.py
+++ src/decorators.py
@@ -2,39 +2,42 @@ from functools import wraps
 from datetime import datetime
 
 
-def log (filename: [str] = None):
+def log (filename:[str] = None):
     def decor(func):
         @wraps(func)
         def wrapp(*args,**kwargs):
-            try :
+            try:
                 start_of_func = datetime.now()
-                res = func(*args,**wargs)
-                end_of_func = datetime.now()
+                start_of_func_= start_of_func.strftime('%Y-%m-%d %H:%M:%S\n')
+                res = func(*args,**kwargs)
                 if filename :
-                     with open ('logs.txt','a',encoding='utf-8') as file:
-                         file.write(f'{function.__name__}ok\n')
-                else:
-                    print(f'{function.__name__}ok\n')
-            except Exception as e:
-                if filename :
-                    with open('logs.txt', 'a', encoding='utf-8') as file:
-                        file.write(start_of_func, end_of_func,type(e))
-                if filename == False:
-                    print(f'error{e}')
-                    print(start_of_func)
-                    print(end_of_func)
+                     with open('logs.txt','a',encoding='utf-8') as file:
+                         file.write(f'{func.__name__} ok\n')
+                if not filename:
+                    print(f'{func.__name__} ok\n')
                 return res
+            except Exception as e:
+                if filename:
+                    with open('logs.txt', 'a', encoding='utf-8') as file:
+                        file.write(start_of_func_)
+                        file.write(f'error{type(e).__name__}\n')
+                if not filename :
+                    print(f'error{type(e).__name__}')
+                    print(start_of_func_)
+                    res=None
+                    return res
 
-            return wrapp
 
-        return decor
+        return wrapp
+
+    return decor
 
 
-@log#(#filename ='logs.txt')
+@log(filename ='logs.txt')
 def functt(a):
-    #""" Функци """
-    #for i in range(a):
-        #print(a)
+    """ Функци """
+    for i in range(a):
+        print(a+c)
     return a
 
 functt(50)
