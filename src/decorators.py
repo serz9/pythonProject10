27157d981bd65db1dc079f04d8fc57c9dbc4a5b9
serz9 <serzkuh@yandex.ru>
2025-06-10 04:22:12 +0500

    some addings

--- src/decorators.py
+++ src/decorators.py
@@ -12,29 +12,29 @@ def log(filename:[str] = None):
                 start_of_func = datetime.now()
                 start_of_func_= start_of_func.strftime('%Y-%m-%d %H:%M:%S:%f\n')
                 end_of_func = datetime.now()
-                end_of_func_ = end_of_func.strftime('%y-%m-%d %H:%M:%S:%f\n')
+                end_of_func_ = end_of_func.strftime('%Y-%m-%d %H:%M:%S:%f\n')
                 res = func(*args,**kwargs)
                 if filename :
                      with open('logs.txt','a',encoding='utf-8') as file:
                          file.write(start_of_func_)
-                         file.write(f'{func.__name__} ok\n')
+                         file.write(f'{func.__name__} ok')
                          file.write(end_of_func_)
                 if not filename:
-                    print(f'{func.__name__} ok\n')
+                    print(f'{func.__name__} ok')
                     print(end_of_func)
                     print(end_of_func_)
                 return res
             except Exception as e:
                 if filename:
                     with open('logs.txt', 'a', encoding='utf-8') as file:
-                        file.write(f'error{type(e).__name__}\n')
+                        file.write(f'error{type(e).__name__}')
                         file.write(start_of_func_)
                         file.write(end_of_func_)
                 if not filename :
                     print(f'error {type(e).__name__}')
                     print(end_of_func_)
                     print(start_of_func_)
-                    res=None
+                    res = None
                     return res
 
 
@@ -50,6 +50,6 @@ def functt(a):
 
     for i in range(a):
         print(a)
-    return a
+    return a+25
 
 functt(50)
