d1f8ec7c1fa427f38c888fdff2efe302981274e3
serz9 <serzkuh@yandex.ru>
2025-09-20 22:52:09 +0500

    some  addings

--- tests/test_mycode.py
+++ tests/test_mycode.py
@@ -1,11 +1,13 @@
 import pytest
 from src.generators import filter_by_currency, transaction_descriptions, tranzactions_list, card_number_generator
-from ..external_api import (convert_transaction, get_exchange_rate)
+from src.external_api.py import (convert_transaction, get_exchange_rate)
 import os
 from src.decorators import log, functt
 from unittest.mock import patch
 from requests.exceptions import HTTPError
 
+a=getcwd()
+print(a)
 
 def test_filter_by_currency():
 
@@ -14,6 +16,9 @@ def test_filter_by_currency():
     res_ = list(filter_by_currency(tranzactions_list,'USD'))
     assert res_ == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
 {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
+
+
+
 
 
 def test_transaction_descriptions():
