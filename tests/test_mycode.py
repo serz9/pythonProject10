35f7f9743e0823abc9d2a1d6fc814ae883d85bf6
serz9 <serzkuh@yandex.ru>
2025-10-02 22:49:57 +0500

    some  addings

--- tests/test_mycode.py
+++ tests/test_mycode.py
@@ -7,15 +7,43 @@ from unittest.mock import patch
 from requests.exceptions import HTTPError
 
 
-
 def test_filter_by_currency():
 
-    """  Тест фильтрации данных  """
+        """  Тест фильтрации данных  """
 
 
-    res_ = filter_by_currency(tranzactions_list,'USD')
-    assert list(res_) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
-{'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
+        res_ = filter_by_currency(tranzactions_list,'USD')
+        assert list(res_) == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": 'RUB',
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }]
 
 
 def test_transaction_descriptions():
@@ -155,4 +183,6 @@ def test_convert_transaction_invalid_data():
 
     result = convert_transaction(invalid_transaction)
     assert result is None
+
+
 
