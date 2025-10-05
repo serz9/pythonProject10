2e6f09ad89fab76d1fab9af926f26c668368904c
serz9 <serzkuh@yandex.ru>
2025-10-05 23:21:07 +0500

    some  addings

--- src/utils.py
+++ src/utils.py
@@ -32,7 +32,7 @@ def jsn_date(path_)  :
     except Exception as e :
         print(f'ошибка {e}')
 
-if  __name__ == '__maine__':
+if  __name__ == '__main__':
 
     jsn_date(r'C:/Users/serzh/PycharmProjects/pythonProject10/date/operations.json')
 
