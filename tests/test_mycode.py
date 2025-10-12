9bc1457d82e0a3d6b67459a1b15f2da038cc9469
serz9 <serzkuh@yandex.ru>
2025-10-13 02:33:25 +0500

    some  addings

--- tests/test_mycode.py
+++ tests/test_mycode.py
@@ -68,3 +68,19 @@ def test_convert_transaction(eur_transaction) :
     with patch('src.external_api.get_exchange_rate',return_value = 75.0):
         res = convert_transaction(eur_transaction)
         assert res == 7500
+
+
+def test_filter_by_currency():
+
+    """  Тест фильтрации данных  """
+
+    res_ = list(filter_by_currency(tranzactions_list,'USD'))
+    assert res_ == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
+{'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
+
+
+def test_transaction_descriptions():
+
+    """   Тест описание транзакций   """
+
+    assert list(transaction_descriptions(tranzactions_list)) == ['Перевод организации', 'Перевод со счета на счет']
