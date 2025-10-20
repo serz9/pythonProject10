c3dcbe4e977474c15cae43c8f1f67caf18bdb893
serz9 <serzkuh@yandex.ru>
2025-10-21 02:42:25 +0500

    some  addings

--- tests/test_mycod.py
+++ tests/test_mycod.py
@@ -3,7 +3,7 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import sort_by_date
 from src.widget import get_time, mask_account_card
-from src.generators import filter_by_currency, get_number_generator, transaction_descriptions, tranzactions_list
+from src.generators import filter_by_currency,card_number_generator, transaction_descriptions, tranzactions_list
 
 def test_get_mask_card_number():
 
