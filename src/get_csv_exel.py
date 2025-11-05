4958a0ff1ca0789720d106ac993b0d76d0f5171a
serz9 <serzkuh@yandex.ru>
2025-11-06 03:07:37 +0500

    some  addings

--- src/get_csv_exel.py
+++ src/get_csv_exel.py
@@ -1,11 +1,24 @@
 import csv
-import get_csv_exel.py
+#import get_csv_exel.py
 #pandas as pd
-#def csv_read():
 
-with open('transactions.csv') as file :
-    reader=csv.reader(file)
+import os
+s = os.getcwd()
+print(s)
+#def csv_read()
+#
+
+
+with open('src.transactions.csv') as file :
+    reader = csv.reader(file)
+
+    next.reader
     for row in reader:
         print(row)
 
-csv_read()
+#csv_read()
+#with open('src.transactions.csv') as file :
+    #reader=csv.reader(file)
+    #for row in reader:
+        #print(row)
+a
