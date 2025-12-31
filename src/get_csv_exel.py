d20662c77e3af460ea99161cfecd5869eaf8c763
serz9 <serzkuh@yandex.ru>
2026-01-01 03:29:06 +0500

    some addings

--- src/get_csv_exel.py
+++ src/get_csv_exel.py
@@ -14,24 +14,26 @@ def read_csv_transactions(path_):
             for row in reader:
                 csv_str = dict(zip(header_, row))
                 csv_dict.append(csv_str)
-
+            print(csv_dict)
             return csv_dict
     except Exception as e:
         print(f"ошибка {e} ")
 
-
+read_csv_transactions(r'C:\\Users\\serzh\\PycharmProjects\\pythonProject10\\src\\transactions.csv')
 def read_excel_transactions(path_):
-    excel_dicts = []
-    reader = pd.read_excel(path_, sheet_name='Лист 1')
-    for index, row in reader.iterrows():
-        head_ = reader.columns.tolist()
-        excel_dict = dict(zip(head_, row))
-        excel_dicts.append(excel_dict)
-    print(excel_dicts)
-    return excel_dicts
+    try:
+        excel_dicts = []
+        reader = pd.read_excel(path_, sheet_name='Лист 1')
+        for index, row in reader.iterrows():
+            head_ = reader.columns.tolist()
+            excel_dict = dict(zip(head_, row))
+            excel_dicts.append(excel_dict)
+        print(excel_dicts)
+        return excel_dicts
+    except Exception as e :
+        print(f"ошибка {e} ")
 
-
-#read_excel_transactions(r'C:\\Users\\serzh\\PycharmProjects\\pythonProject10\\src\\transactions_excel.xlsx')
+read_excel_transactions(r'C:\\Users\\serzh\\PycharmProjects\\pythonProject10\\src\\transactions_excel.xlsx')
 
 if __name__ == "__main__ " :
 
